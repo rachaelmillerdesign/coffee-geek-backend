9f026dca98a010fadde89e9cfc17d199644d2ee6
cpearce31 <cpearce31@gmail.com>
2018-04-17 09:42:00 -0400

    Update from `rails-api-template`
    Refresh for cohort 024.

--- scripts/users/user-update.sh
+++ scripts/users/user-update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-curl "http://localhost:4741/change-password" \
+curl "http://localhost:4741/users/${ID}" \
   --include \
   --request PATCH \
   --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
   --data '{
-    "passwords": {
-      "old": "'"${OLDPW}"'",
-      "new": "'"${NEWPW}"'"
+    "user": {
+      "email": "'"${EMAIL}"'"
     }
   }'
 
