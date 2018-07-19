47b64b39bedcdea9f07e95c99f9fc108c0282194
rachael miller design <rachael@rachaelmillerdesign.com>
2018-07-19 15:41:33 -0400

    Initial commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show update destroy]
 
+  def index
+    render json: Article.all
+  end
+
   private
 
   def set_article
